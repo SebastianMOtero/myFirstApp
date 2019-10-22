9e73a17a4c13abcd2eebacc9b2bc390b3aeaa80a
SebastianMOtero <sebastian.otero@live.com.ar>
2019-10-22 20:54:00 -0300

    22.10.19 [Sprint 1 Epic 5] Implement Router functionality

---
+++ src/CreateAccount.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function CreateAccount() {
+  return (
+    <div className="CreateAccount">
+        <h1>Create Account</h1>
+    </div>
+  );
+}
+
+export default CreateAccount
---
+++ src/MYtinerary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function MYtinerary() {
+  return (
+    <div className="MYtinerary">
+        <h1>MYtinerary</h1>
+    </div>
+  );
+}
+
+export default MYtinerary
